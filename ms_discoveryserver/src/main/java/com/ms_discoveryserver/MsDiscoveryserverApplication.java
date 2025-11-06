2912a38b5be8e1f20c2e0ef8872621f1476c6988
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2025-11-06 11:34:18 -0500

    feat(discoveryserver): Agregar modulo de Eureka server

---
+++ ms_discoveryserver/src/main/java/com/ms_discoveryserver/MsDiscoveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.ms_discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoveryserverApplication.class, args);
+	}
+
+}
