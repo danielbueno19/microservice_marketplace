14f7dc55a147577553b93930f758f4d6a684e546
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2025-11-20 13:06:06 -0500

    feat(ms_apigateway): agregar ruta de microservicio Stok y estandarizar ID de ruta

--- ms_reserva/src/main/java/com/ms_reserva/client/StockClient.java
+++ ms_reserva/src/main/java/com/ms_reserva/client/StockClient.java
@@ -1,12 +1,13 @@
 package com.ms_reserva.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "ms-stock")
 public interface StockClient {
 
-    @RequestMapping("/api/stock/{codigo}")
-    boolean existeStock (@PathVariable String codigo);
+    @GetMapping("/api/stock/{codigo}")
+    boolean isStockDisponible (@PathVariable String codigo);
 }
--- ms_stock/src/main/java/com/ms_stock/MsStockApplication.java
+++ ms_stock/src/main/java/com/ms_stock/MsStockApplication.java
@@ -2,7 +2,9 @@ package com.ms_stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsStockApplication {
 
